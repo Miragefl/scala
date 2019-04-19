8d457ad1a9a71fec5048c2fd665073cf88fce240
fenglei <silence_fl@yeah.net>
2019-04-19 09:17:04 +0800

    Initial commit

---
+++ src/main/scala/indi/matchstick/Test.scala
@@ -0,0 +1,35 @@
+package indi.matchstick
+
+import java.util.Properties
+
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    //    val conf = new SparkConf().setMaster("spark://192.168.1.248:7077").setAppName("rdd");
+    //    val sc = new SparkContext(conf);
+    //    val sqlContext = new SQLContext(sc);
+    //    val url = "jdbc:mysql://47.111.5.179:3306/cc_user_profit?user=root&password=ccxk!123";
+    //    val prop = new Properties();
+    //    val df = sqlContext.read.jdbc(url, "pay_order", prop);
+    //    println("第一种方法输出：" + df.count());
+    //    println("1.------------->" + df.count());
+    //    println("1.------------->" + df.rdd.partitions.size);
+    //    println("1")
+    val spark = SparkSession.builder().appName("MysqlQueryDemo").master("spark://192.168.1.248:7077").getOrCreate()
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://47.111.5.179:3306/cc_user_profit?useUnicode=true&characterEncoding=utf-8")
+      .option("dbtable", "refund_order")
+      .option("user", "root")
+      .option("password", "ccxk!123")
+      .option("driver", "com.mysql.jdbc.Driver")
+      .load()
+    jdbcDF.take(20).foreach(println);
+
+  }
+
+
+}
